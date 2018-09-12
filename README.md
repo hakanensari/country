add109b7a05000c73610ffbd49016de99a215ee7
Hakan Ensari <hakanensari@gmail.com>
2018-09-12 12:14:40 +0100

    Serve all geolocation data

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Country.is
 
-Country.is is a minimal API that tells your browser which country it is in. It uses IP geolocation with [GeoLite2 data](http://dev.maxmind.com/geoip/geoip2/geolite2/) created by MaxMind, so there's no disruptive prompt.
+Country.is is an IP to geolocation API that helps you look up the location of an IP address. It uses [GeoLite2 data](http://dev.maxmind.com/geoip/geoip2/geolite2/) provided by MaxMind.
 
-```sh
+## Usage
+
+The interface is minimal.
+
+The root route queries the IP of your browsing app.
+
+```
+https://api.country.is/
+```
+
+Or specify an IP explicitly.
+
+```
+https://api.country.is/8.8.8.8
+```
+
+## Deployment
+
+Run a private instance with
+
+```
 docker run -d -p 3000:3000 hakanensari/country
 ```
--- app.js
+++ app.js
@@ -4,38 +4,38 @@ const cors = require('cors');
 const express = require('express');
 const maxmind = require('maxmind');
 
-// Seed database file.
-if (!existsSync('./GeoLite2-Country.mmdb')) {
+// Seed database
+if (!existsSync('./GeoLite2-City.mmdb')) {
   spawnSync('./getdb');
 }
 
-// Refresh database file once a day.
+// Refresh database once a day
 setInterval(
   () => { spawn('./getdb'); },
   24 * 3600 * 1000,
 );
 
-// Initialise database.
+// Initialise database
 const countryLookup = maxmind.openSync(
-  './GeoLite2-Country.mmdb',
+  './GeoLite2-City.mmdb',
   { watchForUpdates: true },
 );
 
-// Set up app.
+// Set up app
 const app = express();
 
-app.use(cors());
 app.enable('trust proxy');
+app.use(cors());
 
-app.get('/', (req, res) => {
-  const loc = countryLookup.get(req.ip);
+app.get('/:ip?', (req, res) => {
+  const loc = countryLookup.get(req.params.ip || req.ip);
   if (loc) {
-    res.jsonp({ country: loc.country.iso_code });
+    res.jsonp(loc);
   } else {
-    res.status(404).jsonp({ country: null });
+    res.status(404).jsonp({ error: 'Not found' });
   }
 });
 
-app.listen(3000, () => {
+app.listen(3000, _ => {
   console.log('Listening on port 3000');
 });
