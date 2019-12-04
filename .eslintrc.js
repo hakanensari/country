52e57d053d143c01e11784ac85caa20b51eee9cf
Hakan Ensari <hakanensari@gmail.com>
2019-12-04 15:11:33 +0000

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "rules": {
-    "no-console": 0
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   }
-};
+}
--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const { existsSync } = require('fs');
-const { spawn, spawnSync } = require('child_process');
-const cors = require('cors');
-const express = require('express');
-const maxmind = require('maxmind');
+const { existsSync } = require('fs')
+const { spawn, spawnSync } = require('child_process')
+const cors = require('cors')
+const express = require('express')
+const maxmind = require('maxmind')
 
 // Seed database
 if (!existsSync('./GeoLite2-City.mmdb')) {
-  spawnSync('./getdb');
+  spawnSync('./getdb')
 }
 
 // Refresh database once a day
 setInterval(
-  () => { spawn('./getdb'); },
-  24 * 3600 * 1000,
-);
+  () => { spawn('./getdb') },
+  24 * 3600 * 1000
+)
 
 // Initialise database
 const countryLookup = maxmind.openSync(
   './GeoLite2-City.mmdb',
-  { watchForUpdates: true },
-);
+  { watchForUpdates: true }
+)
 
 // Set up app
-const app = express();
+const app = express()
 
-app.enable('trust proxy');
-app.use(cors());
+app.enable('trust proxy')
+app.use(cors())
 
 app.get('/:ip?', (req, res) => {
-  const loc = countryLookup.get(req.params.ip || req.ip);
+  const loc = countryLookup.get(req.params.ip || req.ip)
   if (loc) {
-    res.jsonp(loc);
+    res.jsonp(loc)
   } else {
-    res.status(404);
+    res.status(404)
   }
-});
+})
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+  console.log('Listening on port 3000')
+})
