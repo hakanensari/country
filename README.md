62aa5f638bd1fe609030c5df57465b67e3d431e2
Hakan Ensari <hakanensari@gmail.com>
2019-12-04 15:46:14 +0000

    Revert to returning country only

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-# Country.is
+# Country
 
-Country.is is an IP to geolocation API that helps you look up the location of an IP address. It uses [GeoLite2 data](http://dev.maxmind.com/geoip/geoip2/geolite2/) provided by MaxMind.
+**Country** is an IP to geolocation API that helps you look up the country where an IP address is based. It uses [GeoLite2 data](http://dev.maxmind.com/geoip/geoip2/geolite2/) provided by MaxMind.
 
 ## Usage
 
 The interface is minimal.
 
-The root route queries the IP of your browsing app.
+The root route queries the IP making the request.
 
 ```
 https://api.country.is/
 ```
 
-Or specify an IP explicitly.
+Or you can specify an IP explicitly.
 
 ```
-https://api.country.is/8.8.8.8
+https://api.country.is/9.9.9.9
 ```
 
 ## Deployment
 
-Run a private instance with
+Use the [hosted service](https://api.country.is) or run privately with
 
 ```
 docker run -d -p 3000:3000 hakanensari/country
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const maxmind = require('maxmind')
 
 // Seed database
-if (!existsSync('./data/GeoLite2-City.mmdb')) {
+if (!existsSync('./data/GeoLite2-Country.mmdb')) {
   spawnSync('./getdb')
 }
 
@@ -16,26 +16,27 @@ setInterval(
 )
 
 // Initialise database
-const countryLookup = maxmind.openSync(
-  './data/GeoLite2-City.mmdb',
+const lookup = maxmind.openSync(
+  './data/GeoLite2-Country.mmdb',
   { watchForUpdates: true }
 )
 
 // Set up app
 const app = express()
 
+app.set('port', process.env.PORT || '3000')
 app.enable('trust proxy')
 app.use(cors())
 
 app.get('/:ip?', (req, res) => {
-  const loc = countryLookup.get(req.params.ip || req.ip)
+  const loc = lookup.get(req.params.ip || req.ip)
   if (loc) {
-    res.jsonp(loc)
+    res.jsonp({ country: loc.country.iso_code })
   } else {
-    res.status(404)
+    res.status(422).send({ country: null })
   }
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}`)
 })
