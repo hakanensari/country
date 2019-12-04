67d0d179150dd7818cef599d80ea30aaf3f59042
Hakan Ensari <hakanensari@gmail.com>
2019-12-04 15:59:45 +0000

    Upgrade maxmind library

--- app.js
+++ app.js
@@ -1,40 +1,30 @@
-const { existsSync } = require('fs')
 const { spawn, spawnSync } = require('child_process')
 const cors = require('cors')
 const express = require('express')
+const { existsSync } = require('fs')
 const maxmind = require('maxmind')
 
-// Seed database
-if (!existsSync('./data/GeoLite2-Country.mmdb')) {
+const datafile = './data/GeoLite2-Country.mmdb'
+
+if (!existsSync(datafile)) {
   spawnSync('./getdb')
 }
+setInterval(() => { spawn('./getdb') }, 24 * 3600 * 1000)
 
-// Refresh database once a day
-setInterval(
-  () => { spawn('./getdb') },
-  24 * 3600 * 1000
-)
-
-// Initialise database
-const lookup = maxmind.openSync(
-  './data/GeoLite2-Country.mmdb',
-  { watchForUpdates: true }
-)
-
-// Set up app
 const app = express()
-
 app.set('port', process.env.PORT || '3000')
 app.enable('trust proxy')
 app.use(cors())
 
 app.get('/:ip?', (req, res) => {
-  const loc = lookup.get(req.params.ip || req.ip)
-  if (loc) {
-    res.jsonp({ country: loc.country.iso_code })
-  } else {
-    res.status(422).send({ country: null })
-  }
+  maxmind.open(datafile).then((lookup) => {
+    const location = lookup.get(req.params.ip || req.ip)
+    if (location) {
+      res.jsonp({ country: location.country.iso_code })
+    } else {
+      res.status(422).send({ country: null })
+    }
+  })
 })
 
 app.listen(app.get('port'), () => {
