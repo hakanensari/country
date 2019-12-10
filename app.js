c9647880460f4af9b23085eafc35e1ed55d4f8dd
Hakan Ensari <hakanensari@gmail.com>
2019-12-10 11:01:03 +0000

    Load data file synchronously

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { spawn, spawnSync } = require('child_process')
 const cors = require('cors')
 const express = require('express')
-const { existsSync } = require('fs')
-const maxmind = require('maxmind')
+const { existsSync, readFileSync } = require('fs')
+const { Reader } = require('maxmind')
 
 const datafile = './data/GeoLite2-Country.mmdb'
 
@@ -10,21 +10,19 @@ if (!existsSync(datafile)) {
   spawnSync('./getdb')
 }
 setInterval(() => { spawn('./getdb') }, 24 * 3600 * 1000)
+const lookup = new Reader(readFileSync(datafile), { watchForUpdates: true })
 
 const app = express()
 app.set('port', process.env.PORT || '3000')
 app.enable('trust proxy')
 app.use(cors())
-
-maxmind.open(datafile, { watchForUpdates: true }).then((lookup) => {
-  app.get('/:ip?', (req, res) => {
-    const location = lookup.get(req.params.ip || req.ip)
-    if (location) {
-      res.jsonp({ country: location.country.iso_code })
-    } else {
-      res.status(422).send({ country: null })
-    }
-  })
+app.get('/:ip?', (req, res) => {
+  const location = lookup.get(req.params.ip || req.ip)
+  if (location) {
+    res.jsonp({ country: location.country.iso_code })
+  } else {
+    res.status(422).send({ country: null })
+  }
 })
 
 app.listen(app.get('port'), () => {
