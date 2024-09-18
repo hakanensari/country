3bf656566cfcd2c8b40c9b19183daabd0f61165f
Hakan Ensari <hakanensari@gmail.com>
2024-09-18 14:45:05 +0200

    Multi core

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const app = require("./app")
+const cluster = require("cluster")
+const os = require("os")
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+if (cluster.isMaster) {
+  // Fork workers
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork()
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  const app = require("./app")
+
+  const port = process.env.PORT || 3000
+  app.listen(port, () => {
+    console.log(`Worker ${cluster.worker.id} listening on port ${port}`)
+  })
+}
